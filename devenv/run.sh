e9f7771826a66f7443282c7bb61ab6159de7d9f6
Dimitris Moraitis <dimitrios.moraitis@dell.com>
2023-11-08 20:18:43 +0200

    Update devenv image, fix authorized_keys list

--- devenv/run.sh
+++ devenv/run.sh
@@ -4,6 +4,6 @@ echo 'source <(kubectl completion bash)
 alias k=kubectl
 complete -F __start_kubectl k
 ' >> $HOME/.bashrc
-echo $authorized_keys > $HOME/.ssh/authorized_keys
+echo -e $authorized_keys > $HOME/.ssh/authorized_keys
 chown -R docker /home/docker
 /usr/sbin/sshd -D
